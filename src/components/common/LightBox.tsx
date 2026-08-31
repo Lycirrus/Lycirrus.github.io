dcf09878e08c687382d88ca9f3742b34ded81b2d
김한송 <knhs4@naver.com>
2026-08-31 19:44:33 +0900

    [Feat] ligh box

--- src/components/common/LightBox.tsx
+++ src/components/common/LightBox.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { createPortal } from "react-dom";
+import { IoCloseCircleOutline } from "react-icons/io5";
 
 interface LightBoxProps {
   src: string;
@@ -17,18 +19,19 @@ const LightBox = ({ src, alt }: LightBoxProps) => {
 
   return (
     <>
-      <div className="md:max-w-3xl aspect-video rounded-xl border border-pf-gray-100 dark:border-pf-gray-300 cursor-zoom-in" onClick={handleOpen}>
+      <div className="sm:max-w-160 aspect-video rounded-xl border border-pf-gray-100 dark:border-pf-gray-300 cursor-zoom-in" onClick={handleOpen}>
         <img src={src} alt={alt} className="w-full h-full object-cover rounded-xl hover:opacity-60 transition-opacity duration-200" />
       </div>
-      {isOpen && (
-        <div className="fixed inset-0 z-100 bg-pf-bg-dark/70 flex items-center justify-center" onClick={handleClose}>
-          <div className="relative w-full max-h-[90dvh]" onClick={(e) => e.stopPropagation()}>
-            <button className="absolute -top-9 right-0 text-white/80 hover:text-white text-sm flex items-center gap-1 transition-colors" onClick={handleClose}>
-              <p>닫기</p>
+      {isOpen && createPortal(
+        <div className="fixed inset-0 z-100 bg-pf-bg-dark/90 flex items-center justify-center" onClick={handleClose}>
+          <div className="relative flex justify-center w-full max-h-[90dvh]" onClick={(e) => e.stopPropagation()}>
+            <button className="absolute -top-8 right-4 text-pf-gray-100 hover:text-pf-white transition-colors" onClick={handleClose}>
+              <IoCloseCircleOutline size={36} />
             </button>
             <img src={src} alt={alt} className="max-w-[90dvw] max-h-[90dvh] object-contain rounded-xl" />
           </div>
-        </div>
+        </div>,
+        document.getElementById("box-root") as HTMLElement
       )}
     </>
   )
